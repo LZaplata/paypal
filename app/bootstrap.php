2aea00bec2607d4c985e6ac3c857ee84173bc874
zdolf <valenta@hucr.cz>
2016-04-21 14:12:29 +0200

    devel mod pro vycet IP adres, kosmetika v kodu

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -22,7 +22,7 @@
 	$configurator->setTempDirectory(__DIR__ . '/../temp');
 
 	// Enable Nette Debugger for error visualisation & logging
-	$configurator->setDebugMode(false);
+	$configurator->setDebugMode('194.12.38.38,188.120.212.32');
 	Debugger::enable(Debugger::DETECT, __DIR__ . '/../log');
 
 	// Enable RobotLoader - this will load all classes automatically
