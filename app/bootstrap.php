9906dbb325c163e28c290d42317b97f8b4db2a18
Zap <zap@hucr.cz>
2016-05-03 11:50:29 +0200

    laděnka

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -22,8 +22,8 @@
 	$configurator->setTempDirectory(__DIR__ . '/../temp');
 
 	// Enable Nette Debugger for error visualisation & logging
-	$configurator->setDebugMode(false);
-	Debugger::enable(Debugger::DETECT, __DIR__ . '/../log');
+	$configurator->setDebugMode(true);
+	Debugger::enable(Debugger::DEVELOPMENT, __DIR__ . '/../log');
 
 	// Enable RobotLoader - this will load all classes automatically
 	$configurator->createRobotLoader()
