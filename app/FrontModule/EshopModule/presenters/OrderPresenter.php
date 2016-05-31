35ce6ae26e44e785728738fb3b3555d30aedb13a
zap <zap@hucr.cz>
2016-05-31 10:20:54 +0200

    překlady

--- app/FrontModule/EshopModule/presenters/OrderPresenter.php
+++ app/FrontModule/EshopModule/presenters/OrderPresenter.php
@@ -424,7 +424,8 @@
 			$form->addText('delivery_street', 'Ulice:')
 				->setDisabled(isset($this->partner->id) ? true : false);
 
-			$form->addText("delivery_street_number", "Číslo popisné:");
+			$form->addText("delivery_street_number", "Číslo popisné:")
+				->setDisabled(isset($this->partner->id) ? true : false);
 
 			$form->addText('delivery_city', 'Město:')
 				->setDisabled(isset($this->partner->id) ? true : false);
