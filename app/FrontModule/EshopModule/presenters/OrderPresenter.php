38f05c6efde2b0c94bbbf7197e8b1ee7c01aef42
zap <zap@hucr.cz>
2016-06-23 14:39:04 +0200

    zadávání psč a telefonu

--- app/FrontModule/EshopModule/presenters/OrderPresenter.php
+++ app/FrontModule/EshopModule/presenters/OrderPresenter.php
@@ -396,7 +396,8 @@
 
 			$form->addText('psc', 'PSČ:')
 				->addRule(Form::PATTERN, 'PSČ musí být ve formátu např. 12-345', '[0-9]{2}[-][0-9]{3}')
-				->setRequired('Vyplňte PSČ!');
+				->setRequired('Vyplňte PSČ!')
+				->setOption("description", "PSČ musí být ve formátu např. 12-345");
 
 			$form->addText('phone', 'Telefon:')
 				->addRule(Form::PATTERN, 'Telefon musí být ve formátu např. 600700800', '[0-9]{9}')
